9c82fd4b5a16268dbf1ebb0b8992bd69baa4a495
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-05-30 16:04:40 -0500

    add very basic json schema markup

---
+++ src/lib/SchemaForm.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { JSONSchema7 } from "json-schema";
+  import Paper, { Title, Subtitle, Content } from "@smui/paper";
+
+  export let schema: JSONSchema7;
+  // export let data: { [prop: string]: any } = {};
+
+  $: title = schema.title;
+  $: description = schema.description;
+</script>
+
+<div>
+  <Paper>
+    <Title>{title}</Title>
+    <Subtitle>{description}</Subtitle>
+    <Content><pre>{JSON.stringify(schema, null, 2)}</pre></Content>
+  </Paper>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import SchemaFrom from "$lib/SchemaForm.svelte";
+  import type { JSONSchema7 } from "json-schema";
+  import testSchema from "../schemas/simple.json";
+
+  const schema = testSchema as JSONSchema7;
+</script>
+
+<SchemaFrom schema={schema} />
