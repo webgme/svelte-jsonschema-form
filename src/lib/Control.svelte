7b93c5ec7def1e131d268b25ed58f03e830828b5
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-05-31 12:58:55 -0500

    add "base" control to delegate to aprropriate control type

---
+++ src/lib/Control.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { JSONSchema7TypeName } from "json-schema";
+  import * as controls from "./controls";
+
+  export let type: JSONSchema7TypeName | JSONSchema7TypeName[] = "object";
+  // export let data: { [prop: string]: any } = {};
+
+  let control: any;
+
+  $: {
+    const singleType = Array.isArray(type) ? type[0] : type;
+    control = controls[singleType as keyof typeof controls] as any;
+  };
+</script>
+
+<svelte:component this={control} {...$$props} />
