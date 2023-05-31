729d7acea6c42883455fdc7d43491755803a3d8e
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-05-31 13:45:46 -0500

    add description to `TextField` based controls

--- src/lib/controls/IntegerControl.svelte
+++ src/lib/controls/IntegerControl.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import Textfield from "@smui/textfield";
+  import HelperText from "@smui/textfield/helper-text";
 
   export let title: string | undefined = undefined;
+  export let description: string | undefined = undefined;
   export let required: boolean | undefined = undefined;
   export let minimum: number | undefined = undefined;
   export let maximum: number | undefined = undefined;
@@ -16,4 +18,8 @@
   input$min={minimum}
   input$max={maximum}
   {required}
-/>
+>
+  {#if description}
+    <HelperText persistent slot="helper">{description}</HelperText>
+  {/if}
+</Textfield>
--- src/lib/controls/StringControl.svelte
+++ src/lib/controls/StringControl.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import Textfield from "@smui/textfield";
+  import HelperText from "@smui/textfield/helper-text";
 
   export let title: string | undefined = undefined;
+  export let description: string | undefined = undefined;
   export let minLength: number | undefined = undefined;
   export let maxLength: number | undefined = undefined;
   export let pattern: string | undefined = undefined;
@@ -17,4 +19,8 @@
   input$maxlength={maxLength}
   input$pattern={pattern}
   {required}
-/>
+>
+  {#if description}
+    <HelperText persistent slot="helper">{description}</HelperText>
+  {/if}
+</Textfield>
