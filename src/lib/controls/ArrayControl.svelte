d91756c76d096b006bbb61c47cd5f13660c8fbe1
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-06-01 15:53:32 -0500

    add basic array control

--- src/lib/controls/ArrayControl.svelte
+++ src/lib/controls/ArrayControl.svelte
@@ -1,3 +1,82 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import type { JSONSchema7 } from "json-schema";
+  import Paper, { Title, Subtitle, Content } from "@smui/paper";
+  import Fab, { Icon } from "@smui/fab";
+  import Control from "../Control.svelte";
 
-<div></div>
+  export let title: string | undefined = undefined;
+  export let description: string | null = null;
+  export let items: JSONSchema7 | JSONSchema7[] | undefined = undefined;
+
+  let itemInstances: JSONSchema7[] = [];
+
+  $: hasItems = itemInstances.length > 0;
+
+  function addItem() {
+    itemInstances = [...itemInstances, items as JSONSchema7];
+  }
+
+  function removeItem(index: number) {
+    itemInstances = itemInstances.filter((_, i) => i !== index);
+  }
+</script>
+
+<Paper class="control-array">
+  <Title>
+    <span>
+      {#if title != null}{title}{/if}
+    </span>
+    {#if items != null}
+      <Fab mini on:click={addItem}>
+        <Icon class="material-icons">add</Icon>
+      </Fab>
+    {/if}
+  </Title>
+  {#if description != null}
+    <Subtitle>{description}</Subtitle>
+  {/if}
+  {#if hasItems}
+    <Content>
+      <ul class="control-array-items">
+        {#each itemInstances as item, index}
+          <li>
+            <Control {...item} />
+            <Fab mini on:click={() => removeItem(index)}>
+              <Icon class="material-icons">delete</Icon>
+            </Fab>
+          </li>
+        {/each}
+      </ul>
+    </Content>
+  {/if}
+</Paper>
+
+<style>
+  :global(.control-array .smui-paper__title) {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .control-array-items {
+    list-style: none;
+    padding-left: 0;
+  }
+
+  .control-array-items > li {
+    display: flex;
+    align-items: center;
+  }
+
+  .control-array-items > li > :global(*:not(.mdc-fab:last-child)) {
+    flex: 1;
+  }
+
+  .control-array-items > li > :global(.mdc-fab:last-child) {
+    margin-left: 1rem;
+  }
+
+  .control-array-items :global(.smui-paper__content) {
+    display: flex;
+  }
+
+</style>
--- src/lib/controls/ObjectControl.svelte
+++ src/lib/controls/ObjectControl.svelte
@@ -2,24 +2,22 @@
   import Paper, { Title, Subtitle, Content } from "@smui/paper";
   import Control from "../Control.svelte";
 
-  export let title: string | null = null;
-  export let description: string | null = null;
+  export let title: string | undefined = undefined;
+  export let description: string | undefined = undefined;
   export let properties: { [prop: string]: any } = {};
   export let required: string[] = [];
 </script>
 
-<div>
-  <Paper>
-    {#if title !== null}
-      <Title>{title}</Title>
-    {/if}
-    {#if description !== null}
-      <Subtitle>{description}</Subtitle>
-    {/if}
-    <Content>
-      {#each Object.entries(properties) as [name, props]}
-        <Control required={required.includes(name)} title={name} {...props} />
-      {/each}
-    </Content>
-  </Paper>
-</div>
+<Paper>
+  {#if title != null}
+    <Title>{title}</Title>
+  {/if}
+  {#if description != null}
+    <Subtitle>{description}</Subtitle>
+  {/if}
+  <Content>
+    {#each Object.entries(properties) as [name, props]}
+      <Control required={required.includes(name)} title={name} {...props} />
+    {/each}
+  </Content>
+</Paper>
