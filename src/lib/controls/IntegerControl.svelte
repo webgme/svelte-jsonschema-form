2f7e58435df8997b49cf0ef2ef7d01b01fa3eca2
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-06-15 16:58:08 -0500

    update integer and number controls to support `enum` and `multipleOf` settings

--- src/lib/controls/IntegerControl.svelte
+++ src/lib/controls/IntegerControl.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Textfield from "@smui/textfield";
   import HelperText from "@smui/textfield/helper-text";
+  import Select, { Option } from "@smui/select";
 
   export let data: number | undefined = undefined;
   export let title: string | undefined = undefined;
@@ -8,12 +9,15 @@
   export let required: boolean | undefined = undefined;
   export let minimum: number | undefined = undefined;
   export let maximum: number | undefined = undefined;
+  export let multipleOf: number | undefined = undefined;
 
   let defaultValue = $$props.default ?? "";
   let value = data ?? defaultValue;
+  let enumValues: number[] | undefined = undefined;
 
   $: updateData(value);
   $: updateValue(data);
+  $: enumValues = $$props.enum;
 
   function updateData(val: number) {
     if ((data !== val) && ((data != null) || (val != defaultValue))) {
@@ -28,17 +32,29 @@
   }
 </script>
 
-<div>
-  <Textfield
-    type="number"
-    label={title}
-    bind:value={value}
-    input$min={minimum}
-    input$max={maximum}
-    {required}
-  >
-    <HelperText persistent slot="helper">
+<div class="control-integer">
+  {#if enumValues?.length}
+    <Select bind:value label={title} {required}>
+      {#each enumValues as enumValue}
+        <Option value={enumValue}>{enumValue}</Option>
+      {/each}
+    </Select>
+    <HelperText persistent>
       {#if description}{description}{/if}
     </HelperText>
-  </Textfield>
+  {:else}
+    <Textfield
+      type="number"
+      label={title}
+      bind:value={value}
+      input$min={minimum}
+      input$max={maximum}
+      input$step={multipleOf}
+      {required}
+    >
+      <HelperText persistent slot="helper">
+        {#if description}{description}{/if}
+      </HelperText>
+    </Textfield>
+  {/if}
 </div>
