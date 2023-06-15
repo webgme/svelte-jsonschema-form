14bf25ed4db0eb34efc0255cdf5d1262cc18cbe5
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-06-15 11:54:19 -0500

    add support for schema references

--- src/lib/SchemaForm.svelte
+++ src/lib/SchemaForm.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
   import type { JSONSchema7 } from "json-schema";
+  import JsonSchemaDereferencer from "@json-schema-tools/dereferencer";
   import Control from "./Control.svelte"; 
 
   export let schema: JSONSchema7 = {};
   export let data: { [prop: string]: any } = {};
+
+  $: dereferencing = new JsonSchemaDereferencer(schema).resolve();
+
 </script>
 
-<Control {...schema} bind:data={data} />
+{#await dereferencing}
+  <div>dereferencing...</div>
+{:then dereferenced}
+  <Control {...dereferenced} bind:data={data} />
+{:catch error}
+  <div class="error">ERROR: {error.message}</div>
+{/await}
