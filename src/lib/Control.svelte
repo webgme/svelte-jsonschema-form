f652f35d037cd544ce2068ea8d79cc749ccefe01
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-07-21 17:44:06 -0500

    allow type passed to `Control` component to be `undefined`
    common for a sub-schema with `anyOf` to not have a "root" type

--- src/lib/Control.svelte
+++ src/lib/Control.svelte
@@ -2,13 +2,13 @@
   import type { JSONSchema7TypeName } from "json-schema";
   import * as controls from "./controls";
 
-  export let type: JSONSchema7TypeName | JSONSchema7TypeName[] = "object";
+  export let type: JSONSchema7TypeName | JSONSchema7TypeName[] | undefined = undefined;
   export let data: any = undefined;
 
   let control: any;
 
   $: {
-    const singleType = Array.isArray(type) ? type[0] : type;
+    const singleType = (Array.isArray(type) ? type[0] : type) ?? "object";
     control = controls[singleType as keyof typeof controls] as any;
   };
 </script>
