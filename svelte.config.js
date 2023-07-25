66af1df47f12489ec4b80c0f45622fd820d6020e
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-07-25 17:28:01 -0500

    set pages repo base path

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,8 @@ import adapterAuto from '@sveltejs/adapter-auto';
 import adapterStatic from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const { npm_config_static } = process.env;
+const { npm_config_static, BASE_PATH } = process.env;
+const isDev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,7 +15,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: (npm_config_static ? adapterStatic : adapterAuto)()
+		adapter: (npm_config_static ? adapterStatic : adapterAuto)(),
+		paths: {
+			base: isDev ? '' : BASE_PATH,
+		}
 	}
 };
 
