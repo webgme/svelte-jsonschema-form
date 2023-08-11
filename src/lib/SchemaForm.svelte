27b92d789cf36678956ee5b5f8de6b44fda5312d
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-08-11 12:57:09 -0500

    add slot for actions

--- src/lib/SchemaForm.svelte
+++ src/lib/SchemaForm.svelte
@@ -2,6 +2,7 @@
   import 'core-js/actual/structured-clone';
   import type { JSONSchema7 } from "json-schema";
   import JsonSchemaDereferencer from "@json-schema-tools/dereferencer";
+  import validator, { ValidationErrors } from "@json-schema-tools/validator";
   import mergeAllOf from "json-schema-merge-allof";
   import Control from "./Control.svelte"; 
 
@@ -15,10 +16,56 @@
    */
   const Dereferencer: typeof JsonSchemaDereferencer = (<any>JsonSchemaDereferencer).default ?? JsonSchemaDereferencer;
 
+  const actions = {
+    get blob() {
+      return getBlob();
+    },
+
+    get url() {
+      return getUrl();
+    },
+
+    validate,
+    download
+  };
+
   $: dereferencing = new Dereferencer(
     mergeAllOf(structuredClone(schema)),
     { mutate: true }
   ).resolve();
+
+  async function validate() {
+    const schema = await dereferencing;
+    return validator(schema, data);
+  }
+
+  function getBlob() {
+    return new Blob(
+      [JSON.stringify(data)],
+      { type: "application/json"}
+    );
+  }
+
+  function getUrl() {
+    const blob = getBlob();
+    return URL.createObjectURL(blob);
+  }
+
+  async function download(filename: string = "data", options?: { validate: boolean }) {
+    const opts = Object.assign({ validate: true }, options);
+    const validity = opts.validate && await validate();
+    if (validity instanceof ValidationErrors) {
+      throw validity;
+    }
+    const url = getUrl();
+    const anchor = document.createElementNS('http://www.w3.org/1999/xhtml', 'a') as HTMLAnchorElement;
+    anchor.href = url;
+    anchor.setAttribute("download", `${filename}.json`);
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+    URL.revokeObjectURL(url);
+  }
 </script>
 
 <form class="jsonschema-form">
@@ -29,5 +76,9 @@
   {:catch error}
     <div class="error">ERROR: {error.message}</div>
   {/await}
+
+  <div class="actions">
+    <slot {actions} />
+  </div>
 </form>
  
