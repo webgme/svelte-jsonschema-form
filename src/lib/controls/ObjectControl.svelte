757f67b36174828c2fe3cb8d18ad4def6f9825c9
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-08-18 12:56:51 -0500

    only show `object` if has properties

--- src/lib/controls/ObjectControl.svelte
+++ src/lib/controls/ObjectControl.svelte
@@ -13,44 +13,49 @@
   export let anyOf: JSONSchema7Definition[] | undefined = undefined;
 
   let open = true;
+  let hasProps = false;
 
   $: if (data == null) {
     data = {};
   }
-
   $: justAnyOf = (title == null) && (properties == null) && (anyOf != null);
+  $: hasProps = !!Object.keys(properties ?? {}).length || !!Object.keys(anyOf ?? {}).length;
 </script>
 
-{#if justAnyOf}
-  <AnyOfControl {anyOf} type={'object'} bind:data={data} />
-{:else}
-  <Accordion class="control-object">
-    <Panel bind:open color="secondary">
-      <Header>
-        {title ?? ""}
-        <span slot="description">{description ?? ""}</span>
-        <IconButton slot="icon" toggle pressed={open}>
-          <Icon class="material-icons" on>expand_less</Icon>
-          <Icon class="material-icons">expand_more</Icon>
-        </IconButton>
-      </Header>
-      <Content color="white">
-        {#if properties != null}
-          {#each Object.entries(properties) as [name, props] (name)}
-            <Control
-              isRequired={required.includes(name)}
-              title={name}
-              {...props}
-              bind:data={data[name]}
-            />
-          {/each}
-        {/if}
-        {#if anyOf != null}
-          <AnyOfControl {anyOf} type={'object'} bind:data={data} />
-        {/if}
-      </Content>
-    </Panel>
-  </Accordion>
+{#if hasProps}
+  {#if justAnyOf}
+    <AnyOfControl {anyOf} type={'object'} bind:data={data} />
+  {:else}
+    <Accordion class="control-object">
+      <Panel bind:open>
+        <slot name="title" {title}>
+          <Header>
+            {title ?? ""}
+            <span slot="description">{description ?? ""}</span>
+            <IconButton slot="icon" toggle pressed={open}>
+              <Icon class="material-icons" on>expand_less</Icon>
+              <Icon class="material-icons">expand_more</Icon>
+            </IconButton>
+          </Header>
+        </slot>
+        <Content color="white">
+          {#if properties != null}
+            {#each Object.entries(properties) as [name, props] (name)}
+              <Control
+                isRequired={required.includes(name)}
+                title={name}
+                {...props}
+                bind:data={data[name]}
+              />
+            {/each}
+          {/if}
+          {#if anyOf != null}
+            <AnyOfControl {anyOf} type={'object'} bind:data={data} />
+          {/if}
+        </Content>
+      </Panel>
+    </Accordion>
+  {/if}
 {/if}
 
 
