73c47753c310bc4bed7c54c3b42261bf54f6b2aa
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-08-23 15:04:21 -0500

    add ui-schema definition & utilities

---
+++ src/lib/UISchema.ts
@@ -0,0 +1,35 @@
+import type { JSONSchema7 } from "json-schema";
+import { getContext, setContext } from 'svelte';
+import { writable, type Readable } from "svelte/store";
+import { hasRequired } from "./utilities";
+
+type UISchema = {
+  collapse?: "all" | "none" | "unrequired"
+};
+
+namespace UISchema {
+  export const Key = Symbol("svelte-jsonschema-form UI schema context key");
+
+  export function get() {
+    return getContext<Readable<UISchema>>(Key);
+  }
+
+  export function store(uischema: UISchema) {
+    const store = writable(uischema);
+    return setContext(Key, store);
+  }
+
+  export function shouldCollapse(schema: JSONSchema7, setting: UISchema['collapse'], fallback: boolean) {
+    switch (setting) {
+      case "all":
+        return true;
+      case "none":
+        return false;
+      case "unrequired":
+        return hasRequired(schema);
+    }
+    return fallback;
+  }
+}
+
+export default UISchema;
--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -1,4 +1,5 @@
 import type { JSONSchema7 } from "json-schema";
+import traverse from "@json-schema-tools/traverse";
 
 export function toClasses(classObj: { [cls: string]: boolean }) {
   return Object.entries(classObj)
@@ -9,4 +10,12 @@ export function toClasses(classObj: { [cls: string]: boolean }) {
 
 export function isObjectSchema(schema: JSONSchema7) {
   return schema.type === "object";
+}
+
+export function hasRequired(schema: JSONSchema7) {
+  let hasRequired = false;
+  traverse(schema, schemaOrSubschema => {
+    hasRequired ||= schemaOrSubschema.required?.length > 0;
+  });
+  return hasRequired;
 }
