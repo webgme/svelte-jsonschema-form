2b39512a45765c9017969feb4cd68b4e2e2d474a
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-08-30 12:10:49 -0500

    add `description` support for boolean types

--- src/lib/controls/BooleanControl.svelte
+++ src/lib/controls/BooleanControl.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import FormField from "@smui/form-field";
   import Checkbox from "@smui/checkbox";
+  import HelperText from "@smui/textfield/helper-text";
 
   export let data: boolean | undefined = undefined;
   export let title: string | undefined = undefined;
+  export let description: string | undefined = undefined;
   export let force: boolean = false;
 
   let defaultValue = $$props.default ?? false;
@@ -26,7 +28,19 @@
 
 </script>
 
-<FormField class="jsonschema-form-control control-boolean">
-  <Checkbox bind:checked />
-  <span slot="label">{title}</span>
-</FormField>
+<div class="jsonschema-form-control control-boolean">
+  <FormField>
+    <Checkbox bind:checked />
+    <span slot="label">{title}</span>
+  </FormField>
+  {#if description}
+    <HelperText persistent>{description}</HelperText>
+  {/if}
+</div>
+
+<style>
+  .control-boolean > :global(.mdc-text-field-helper-text) {
+    margin-top: -10px;
+    margin-left: 10px;
+  }
+</style>
