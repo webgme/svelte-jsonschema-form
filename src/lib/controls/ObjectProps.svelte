021542183db75fe218b75018beb618faa21debb7
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-09-26 15:50:01 -0500

    update `ObjectProps` control iteration

--- src/lib/controls/ObjectProps.svelte
+++ src/lib/controls/ObjectProps.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import type { JSONSchema7Definition } from "json-schema";
+  import type { JSONSchema7, JSONSchema7Definition } from "json-schema";
   import type UISchema from "$lib/UISchema";
   import Control from "../Control.svelte";
   import AnyOfControl from "./AnyOfControl.svelte";
@@ -9,12 +9,23 @@
   export let properties: { [prop: string]: any } | undefined = undefined;
   export let required: string[] = [];
   export let anyOf: JSONSchema7Definition[] | undefined = undefined;
+
+  let items = [] as { name: string, schema: JSONSchema7, uischema: UISchema }[];
+
+  $: updateItems(properties, uischema);
+
+  function updateItems(properties: { [prop: string]: any } | undefined, uischema: UISchema) {
+    items = (properties == null) ? [] :
+      Object.entries(properties).map(([name, props]) => {
+        const schema = {title: name, ...props, isRequired: required.includes(name)};
+        return { name, schema, uischema: uischema?.[name] };
+      });
+  }
 </script>
 
-{#if (properties != null) && (data != null)}
-  {#each Object.entries(properties) as [name, props] (name)}
-    {@const schema = {title: name, ...props, isRequired: required.includes(name)}}
-    <Control {schema} bind:data={data[name]} uischema={uischema[name]} />
+{#if !!data}
+  {#each items as { name, schema, uischema } (name)}
+    <Control {schema} bind:data={values[name]} {uischema} />
   {/each}
 {/if}
 {#if (anyOf != null)}
