4c673a1bfb48798449ded45a351d3aa20289bc24
Brian Broll <brian.broll@gmail.com>
2023-10-02 13:52:18 -0500

    Add test stubs

---
+++ tests/anyOf.ts
@@ -0,0 +1,24 @@
+import { test } from "@playwright/test";
+
+test.describe("anyOf", function () {
+  test("hide (empty) field header", async ({ _page }) => {
+    // TODO: create an enum
+    // TODO: select an option
+    // TODO: ensure the option doesn't show again below the header
+    test.fixme();
+  });
+
+  test("show nested field options", async ({ _page }) => {
+    // TODO: create an enum
+    // TODO: select an option (with nested fields)
+    // TODO: ensure the fields are shown
+    test.fixme();
+  });
+
+  test("set nested field options", async ({ _page }) => {
+    // TODO: create an enum
+    // TODO: select an option (with nested fields)
+    // TODO: ensure the fields can be set
+    test.fixme();
+  });
+});
