c446f6269d44b27585d417d28a2d4e41b856d652
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-11-01 11:47:16 -0500

    add check for `anyOf` to `Control` component

--- src/lib/Control.svelte
+++ src/lib/Control.svelte
@@ -12,9 +12,18 @@
 
   $: updateControlType(schema);
 
+  function useConst(schema: JSONSchema7 | undefined) {
+    return (schema != null) && ("const" in schema);
+  }
+
+  function useAnyOf(schema: JSONSchema7 | undefined) {
+    return (schema != null) && (("anyOf" in schema) && !("properties" in schema));
+  }
+
   function updateControlType(schema: JSONSchema7 | undefined) {
-    const hasConst = (schema != null) && Object.hasOwn(schema, "const");
-    const type = hasConst ? "const" : schema?.type;
+    const type = useConst(schema) ? "const"
+      : useAnyOf(schema) ? "anyOf"
+      : schema?.type;
     const singleType = (Array.isArray(type) ? type[0] : type) ?? "object";
     control = controls[singleType as keyof typeof controls] as any;
   }
