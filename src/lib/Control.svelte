b23d0419c72d959a5ba63372366c12a4c43a1d97
Sean Wilson <sean.c.wilson@vanderbilt.edu>
2023-11-03 10:54:37 -0500

    add schema boolean check for `Control`'s `useConst`

--- src/lib/Control.svelte
+++ src/lib/Control.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { JSONSchema7 } from "json-schema";
   import type UISchema from "./UISchema";
+  import { isBoolean } from "./utilities";
   import * as controls from "./controls";
 
   export let schema: JSONSchema7 | undefined;
@@ -13,7 +14,7 @@
   $: updateControlType(schema);
 
   function useConst(schema: JSONSchema7 | undefined) {
-    return (schema != null) && ("const" in schema);
+    return (schema != null) && !isBoolean(schema) && ("const" in schema);
   }
 
   function useAnyOf(schema: JSONSchema7 | undefined) {
