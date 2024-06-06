aba981b4410f57d80179f77cb329af6236ecbc10
Sean Wilson <contact@seanwilson.dev>
2024-06-06 11:11:45 -0500

    fix issue w/ `import.meta.env` not existing w/o bundler

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,1 +1,2 @@
-export default (import.meta.env.version = __SVELTE_JSONSCHEMA_FORM_VERSION__ ) as string;
+const env = import.meta?.env ?? {};
+export default (env.version = __SVELTE_JSONSCHEMA_FORM_VERSION__ ) as string;
